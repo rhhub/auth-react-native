8ebb85ade921f165f8eda6a57876f40389599472
Ryan <ryanhuebert@gmail.com>
2017-06-25 10:09:33 -0700

    indexed common components

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+
+// need to use a named export instead of a default export in containing components
