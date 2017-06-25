958d9ab1e3420c8aac040444114948c0bb2d0b56
Ryan <ryanhuebert@gmail.com>
2017-06-25 10:33:28 -0700

    add firebase dependency and init in App.js componentWillMount()

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBW0Sb1wp0cmNhzx6V3D424Rthi_10o-KU',
+      authDomain: 'auth-13e69.firebaseapp.com',
+      databaseURL: 'https://auth-13e69.firebaseio.com',
+      projectId: 'auth-13e69',
+      storageBucket: 'auth-13e69.appspot.com',
+      messagingSenderId: '591760752756'
+  });
+  }
+
   render() {
     return (
       <View>
